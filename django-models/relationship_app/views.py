ba702cc39b959ca3460e973165a879b1da3f1ac6
Joel Stephen <joel.stephen56.sj@gmail.com>
2024-08-18 23:25:00 +0100

    fixes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -40,8 +40,7 @@ class UserRegisterView(CreateView):
 
 
 # Helper functions for role checks
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
+
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
@@ -49,10 +48,7 @@ def is_librarian(user):
 def is_member(user):
     return user.userprofile.role == 'Member'
 
-# Admin View
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+
 
 
 # Librarian View
