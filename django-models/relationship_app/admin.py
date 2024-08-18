d1044d480afac82049cb78c6876f7e013203e28f
Joel Stephen <joel.stephen56.sj@gmail.com>
2024-08-18 23:23:43 +0100

    fix

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -7,3 +7,13 @@ admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
 admin.site.register(Author)
+
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
